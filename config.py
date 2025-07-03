7fae8240269673c3bf23e1c7dcb07c969459af61
Vitalii Filonov <vitalii.filonov@outlook.com>
2025-07-03 20:15:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
-BOT_TOKEN = "7571403720:AAHze0n8PDBmrv7wrKxE7NFCRdagDgL1wac" # Telegram Bot token
-ADMIN_ID = 502748595  # Telegram ID
-AGENT_ID = "ag:859ee711:20250408:untitled-agent:79fcec57" # Mistral agent ID
-API_URL = "https://api.mistral.ai/v1/agents/completions" # Mistral API URL
-API_KEY = "TzyjIxcObFjasRHigpqZyMnwStyIRYpB" # Mistral API key
+BOT_TOKEN = "********************" # Telegram Bot token
+ADMIN_ID = **********  # Telegram ID
+AGENT_ID = "***********************" # Mistral agent ID
+API_URL = "************************" # Mistral API URL
+API_KEY = "***********************" # Mistral API key
